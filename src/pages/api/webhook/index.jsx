813e612312a9bb48633bb8feb61dfb3cb2835884
Talent Service <talentservice129@gmail.com>
2022-09-22 07:33:53 +0600

    node version

--- src/pages/api/webhook/index.jsx
+++ src/pages/api/webhook/index.jsx
@@ -6,7 +6,7 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: '2020-03-02',
+  apiVersion: '2022-08-01',
 });
 
 // Stripe requires the raw body to construct the event.
